d4df7f990b7de5e88279d16a7c632b6a0d1af267
FlareItsh <rayverdasalla@gmail.com>
2026-04-01 21:30:49 +0800

    feat: implement user authentication and logout functionality via AuthController and UserService

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Service\UserService;
 
 class AuthController extends Controller
 {
-    //
+    private UserService $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    public function login(Request $request)
+    {
+        return $this->userService->loginUser($request);
+    }
+
+    public function logout(Request $request)
+    {
+        return $this->userService->logoutUser($request->user());
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/test', function () {
-    return response()->json([
-        'message' => 'Test API is working'
-    ]);
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('users', UserController::class);
 });
